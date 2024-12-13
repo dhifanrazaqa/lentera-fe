4f60ba02346b0fb4a2eaebc8ddf29d7ec6c5ab22
dhifanrazaqa <dhifanofficial@gmail.com>
2024-12-14 00:01:32 +0700

    update api config

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://lentera-be.vercel.app",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
